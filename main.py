11bb8130a609538189556a7875172a3d7feaad59
Diego Lopes <dieolpes77@gmail.com>
2023-05-21 17:07:21 -0300

    feito

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def count_vowels(string:str) -> int:
     Returns:
         int: The number of vowels in the string.
     """
+
+
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
